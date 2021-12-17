4e578f7f00a4e3976b74aac33b7a27ca03f30d99
Reinoptland <reinoptland@gmail.com>
2021-12-17 20:30:26 +0100

    Define multiply function

--- functions/calculator.js
+++ functions/calculator.js
@@ -7,8 +7,11 @@ function subtract(num1, num2) {
   return num1 - num2;
 }
 
+function multiply() {}
+
 module.exports = {
   add,
   subtract,
+  multiply,
   // Export the add, subtract, multiply, divide and calculate functions here
 };

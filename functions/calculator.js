f49c50c20c8d006a55e91d2d36655ac28d8141ab
Reinoptland <reinoptland@gmail.com>
2021-12-17 20:29:18 +0100

    Define subtract function and export it

--- functions/calculator.js
+++ functions/calculator.js
@@ -3,7 +3,10 @@ function add(num1, num2) {
   return num1 + num2;
 }
 
+function subtract() {}
+
 module.exports = {
   add,
+  subtract,
   // Export the add, subtract, multiply, divide and calculate functions here
 };

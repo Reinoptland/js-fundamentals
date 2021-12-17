fce4a0ece1676b1fb1e0bf5cdb216eb0a918c470
Reinoptland <reinoptland@gmail.com>
2021-12-17 20:48:06 +0100

    Call the right function depending on the operation

--- functions/calculator.js
+++ functions/calculator.js
@@ -15,7 +15,21 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-function calculate(num1, operation, num2) {}
+function calculate(num1, operation, num2) {
+  switch (operation) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+
+    default:
+      break;
+  }
+}
 
 module.exports = {
   add,

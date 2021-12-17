e6c7bed39bb385491d57d50f5153db250e4e9933
Reinoptland <reinoptland@gmail.com>
2021-12-17 20:31:34 +0100

    Define divide function and export it

--- functions/calculator.js
+++ functions/calculator.js
@@ -11,9 +11,12 @@ function multiply(num1, num2) {
   return num1 * num2;
 }
 
+function divide() {}
+
 module.exports = {
   add,
   subtract,
   multiply,
+  divide,
   // Export the add, subtract, multiply, divide and calculate functions here
 };

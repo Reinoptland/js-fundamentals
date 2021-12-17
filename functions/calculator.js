6d4a1e3defa936ac4ae9cfd3f9762926a85cf98c
Reinoptland <reinoptland@gmail.com>
2021-12-17 20:27:39 +0100

    Define add function

--- functions/calculator.js
+++ functions/calculator.js
@@ -1,5 +1,7 @@
 // Write the add, subtract, multiply, divide and calculate functions here
+function add() {}
 
 module.exports = {
+  add,
   // Export the add, subtract, multiply, divide and calculate functions here
 };

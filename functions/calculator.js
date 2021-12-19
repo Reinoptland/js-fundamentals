a136841a2603e4c5aaa8653bad2605132dcbd3ad
Reinoptland <reinoptland@gmail.com>
2021-12-19 18:27:56 +0100

    Solved add tests, feedback plz!

--- functions/calculator.js
+++ functions/calculator.js
@@ -1,5 +1,9 @@
 // Write the add, subtract, multiply, divide and calculate functions here
+function add(num1, num2) {
+  return num1 + num2;
+}
 
 module.exports = {
+  add: add,
   // Export the add, subtract, multiply, divide and calculate functions here
 };

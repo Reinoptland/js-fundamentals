c7483a1e009a0eac030b1a4439eca26ed7f05883
Harm de Kluijver <harmdekluijver@MacBook-Pro-van-Harm.local>
2022-01-24 14:36:03 +0100

    Ben benieuwd!

--- functions/calculator.js
+++ functions/calculator.js
@@ -1,5 +1,61 @@
 // Write the add, subtract, multiply, divide and calculate functions here
 
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+
+function calculate(num1, oper, num2) {
+
+  switch ( oper ) {
+
+    case "+":
+      return num1 + num2;
+
+      break;
+
+    case "-":
+      return num1 - num2;
+
+      break;
+
+    case "*":
+      return num1 * num2;
+
+      break;
+
+    case "/":
+      return num1 / num2;
+
+      break;
+
+
+    default:
+      return NaN;
+
+      break;
+
+  }
+}
+
+
 module.exports = {
   // Export the add, subtract, multiply, divide and calculate functions here
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+    calculate: calculate,
 };
